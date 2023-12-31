ed28d939422732b07d551014334dfe4e0d2ea1cc
Roze Aranbayev <roz17052006@gmail.com>
2023-12-31 15:48:31 +0200

    Initial code +Welcome functions

---
+++ Stage0/Program7970.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Stage0;
+
+partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome7970();
+        Welcome5202();
+        Console.ReadKey();
+    }
+
+    static partial void Welcome5202();
+    private static void Welcome7970()
+    {
+        Console.WriteLine("Enter your name: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", userName);
+    }
+}
