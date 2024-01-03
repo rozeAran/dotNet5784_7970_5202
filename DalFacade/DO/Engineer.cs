fd6c49cb6fd501754e688b9b5a1f81a77271cb94
tal217 <tal217@ultalya.com>
2024-01-03 22:01:20 +0200

    talllll

--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -1,5 +1,6 @@
 ﻿
 namespace DO;
+
 /// <summary>
 /// represents an engineer that works on a task and includes usefull information about him/her
 /// </summary>
@@ -8,10 +9,12 @@ public record Engineer
     int Id,//engineer's id
     string Name,//engineer's name
     string Email,//engineer's mail
-    DO.EngineerExperience Level,//engineer's experience
+    EngineerExperience Level,//engineer's experience
     double Cost//engineer's salary
 )
 
 {
+    public Engineer() : this(1,"","", ,1) { }//deafult constractor
+   
+ }
 
-}
--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -5,4 +5,19 @@ static class DataSource
     internal static List<DO.Task> Tasks { get; } = new();
     internal static List<DO.Engineer> Engineers { get; } = new();
     internal static List<DO.Dependency> Dependencies { get; } = new();
+    internal static class Config
+    {
+        internal const int startDepId = 1000;
+        private static int nextDepId = startDepId;
+        internal static int NextDepId { get => nextDepId++; }
+        //...
+    }
+    internal static class Config
+    {
+        internal const int startTaskId = 1000;
+        private static int nextTaskId = startTaskId;
+        internal static int NextTaskId { get => nextTaskId++; }
+        //...
+    }
+
 }
