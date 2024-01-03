8652b85cf83c6c4e443ded78e4449a33d669c684
Roze Aranbayev <roz17052006@gmail.com>
2024-01-03 23:35:19 +0200

    resd

--- DalList/DependencyImplementation.cs
+++ DalList/DependencyImplementation.cs
@@ -2,6 +2,7 @@
 using DalApi;
 using DO;
 using System.Collections.Generic;
+using System.Linq;
 
 public class DependencyImplementation : IDependency
 {
@@ -13,16 +14,22 @@ public class DependencyImplementation : IDependency
     public void Delete(int id)
     {
         throw new NotImplementedException();
+        
     }
 
     public Dependency? Read(int id)
     {
         throw new NotImplementedException();
+       // Dependency temp=new Dependency(id,0,0);
+        return DataSource.Dependencies.Find(x => x.Id == id);
+
+
     }
 
     public List<Dependency> ReadAll()
     {
         throw new NotImplementedException();
+        return new List<T>(DataSource.Ts);
     }
 
     public void Update(Dependency item)
