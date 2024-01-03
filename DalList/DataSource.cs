e8af3ff21e12a6edba2c8af44bd0a6eb237a3dbe
Roze Aranbayev <roz17052006@gmail.com>
2024-01-03 21:33:05 +0200

    DATASOURCE

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -2,4 +2,7 @@
 
 static class DataSource
 {
+    internal static List<DO.Task> Tasks { get; } = new();
+    internal static List<DO.Engineer> Engineers { get; } = new();
+    internal static List<DO.Dependency> Dependencies { get; } = new();
 }
