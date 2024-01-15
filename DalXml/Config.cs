ea6b21e5b367b86e92271d7c7aae07f021460ad8
tal217 <tal217@ultalya.com>
2024-01-15 18:03:02 +0200

    fdch

---
+++ DalXml/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Xml.Linq;
+namespace Dal;
+
+internal static class Config
+{
+    static string s_data_config_xml = "data-config";
+    internal static int NextDepId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextDepId"); }
+
+    internal static int NextTaskId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextTaskId"); }
+   
+
+}
