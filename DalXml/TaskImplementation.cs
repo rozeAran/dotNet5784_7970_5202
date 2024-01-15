87686a447cba3327cba9c57963ce379ab8941450
Roze Aranbayev <roz17052006@gmail.com>
2024-01-15 22:37:09 +0200

    hh

--- DalXml/TaskImplementation.cs
+++ DalXml/TaskImplementation.cs
@@ -2,6 +2,7 @@
 using Dal;
 using DO;
 using System.Xml.Serialization;
+using System.Data.Common;
 namespace Dal;
 
 internal class TaskImplementation : ITask
@@ -10,7 +11,32 @@ internal class TaskImplementation : ITask
 
     public int Create(DO.Task item)
     {
+        int id = Config.NextTaskId;
+        DO.Task copy = item with { Id = id };
+        List<DO.Task> tasks = new List<DO.Task>();
+        XmlSerializer Deserialize = new XmlSerializer(typeof(DO.Task));
+        if (tasks.Count > 0 )
+             tasks.Deserialize(s_tasks_xml);
+        tasks.Add(item);
         throw new NotImplementedException();
+        DO.Task copy = item with { Id = item.Id};
+
+        // יצירת אינסטנס של XmlSerializer עבור המחלקה Task
+        XmlSerializer serializer = new XmlSerializer(typeof(Task));
+
+        // יצירת Stream חדש עבור הכתיבה לקובץ
+        using (StreamWriter writer = new StreamWriter("tasks.xml"))
+        {
+            // הכתיבה של המשימה לתוך הקובץ באמצעות XmlSerializer
+            serializer.Serialize(writer, copy);
+        }
+
+        DataSource.Tasks.Add(copy);
+        return copy.Id;
+        /*        int id = DataSource.Config.NextTaskId;
+        Task copy = item with { Id = id };
+        DataSource.Tasks.Add(copy);
+        return item.Id;*/
     }
 
     public void Delete(int id)
