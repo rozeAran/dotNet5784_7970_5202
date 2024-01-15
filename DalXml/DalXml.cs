a4ba034ffa4f22dda80bef288ee3dc05fb36b1e7
Roze Aranbayev <roz17052006@gmail.com>
2024-01-15 19:18:36 +0200

    ytrhs

---
+++ DalXml/DalXml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DalApi;
+namespace Dal;
+
+
+//stage 3
+sealed public class DalXml : IDal
+{
+    public IEngineer Engineer => new EngineerImplementation();
+    public ITask Task => new TaskImplementation();
+    public IDependency Dependency => new DependencyImplementation();
+}
+
+
