7e28b29ee211b9313c434ab4588c5f9e5b4b3e7e
tal217 <tal217@ultalya.com>
2024-01-16 10:50:58 +0200

    ftuf

--- DalXml/TaskImplementation.cs
+++ DalXml/TaskImplementation.cs
@@ -12,38 +12,13 @@ internal class TaskImplementation : ITask
 
     public int Create(DO.Task item)
     {
+        List<DO.Task> tasks = XMLTools.LoadListFromXMLSerializer<DO.Task>(s_tasks_xml);
         int id = Config.NextTaskId;
         DO.Task copy = item with { Id = id };
-        List<DO.Task> tasks = new List<DO.Task>();
-        tasks= LoadListFromXMLSerializer(s_tasks_xml);
-        //FileStream fs = new FileStream(path, FileMode.Open);
-        XmlSerializer x = new XmlSerializer(typeof(List<DO.Task>));
-         (List<DO.Task>)x.Deserialize(s_tasks_xml);
+        tasks.Add(copy);
+        XMLTools.SaveListToXMLSerializer(tasks, s_tasks_xml);
+        return id;
 
-
-        XmlSerializer Deserialize = new XmlSerializer(typeof(DO.Task));
-        if (tasks.Count > 0 )
-             tasks.Deserialize(s_tasks_xml);
-        tasks.Add(item);
-        throw new NotImplementedException();
-        DO.Task copy = item with { Id = item.Id};
-
-        // יצירת אינסטנס של XmlSerializer עבור המחלקה Task
-        XmlSerializer serializer = new XmlSerializer(typeof(Task));
-
-        // יצירת Stream חדש עבור הכתיבה לקובץ
-        using (StreamWriter writer = new StreamWriter("tasks.xml"))
-        {
-            // הכתיבה של המשימה לתוך הקובץ באמצעות XmlSerializer
-            serializer.Serialize(writer, copy);
-        }
-
-        DataSource.Tasks.Add(copy);
-        return copy.Id;
-        /*        int id = DataSource.Config.NextTaskId;
-        Task copy = item with { Id = id };
-        DataSource.Tasks.Add(copy);
-        return item.Id;*/
     }
 
     public void Delete(int id)
